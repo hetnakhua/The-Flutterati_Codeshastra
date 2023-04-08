aebdb24bc2bd76dab429dd3663b60a36d3ae7564
Nishtha <nishthachitalia0309@gmail.com>
2023-04-08 17:54:36 +0530

    added forum pages

---
+++ lib/screens/Discussion/add_post.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutterati_codeshastra/services/forumdb.dart';
+import 'package:flutterati_codeshastra/util/re_use.dart';
+
+class AddPost extends StatefulWidget {
+  const AddPost({Key? key}) : super(key: key);
+
+  @override
+  State<AddPost> createState() => _AddPostState();
+}
+
+class _AddPostState extends State<AddPost> {
+  TextEditingController _post = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          inputText('Post', 'Enter post', _post, false),
+          TextButton(onPressed: ()
+              async {
+                await addPost(_post.text);
+              }, child: Text('Add'))
+        ],
+      ),
+    );
+  }
+}
