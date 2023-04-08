4db775e10d52fa02e0be7a3a0a0091400d33ed29
Nishtha <nishthachitalia0309@gmail.com>
2023-04-08 17:45:23 +0530

    added forum pages

---
+++ lib/services/forumdb.dart
@@ -0,0 +1,22 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutterati_codeshastra/models/forumpost.dart' as f;
+
+Future addPost(String text) async{
+  try{
+    final id = (FirebaseAuth.instance.currentUser?.uid).toString();
+    final f.ForumPostEntry post = f.ForumPostEntry(
+      username: id,
+      dislikes: 0,
+      likes: 0,
+      text: text,
+      time: DateTime.now().toString(),
+    );
+
+    final json = post.toJson();
+    await FirebaseFirestore.instance.collection('posts').doc(id).set(json);
+  }
+  catch(e){
+    print("$e");
+  }
+}
