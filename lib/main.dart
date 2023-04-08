acdd0fc1d31e36187cf01502ce65b894c0831665
Vatsal Shah <vsshah2003@gmail.com>
2023-04-08 16:53:54 +0530

    Auth page completed

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutterati_codeshastra/constants/colors.dart';
+import 'package:flutterati_codeshastra/screens/Auth/auth.dart';
 import 'package:flutterati_codeshastra/screens/Onboarding/intro_page_1.dart';
 import 'package:flutterati_codeshastra/screens/Profile/login.dart';
 import 'package:flutterati_codeshastra/screens/Profile/register.dart';
@@ -11,33 +12,36 @@ import 'package:google_fonts/google_fonts.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   FirebaseApp firebaseapp = await Firebase.initializeApp();
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+  final appColorScheme = ColorScheme(
+      primary: midnightGreenLight,
+      error: pink,
+      onError: pink,
+      secondary: midnightGreen,
+      onPrimary: midnightGreen,
+      onSecondary: midnightGreenLight,
+      background: raisinBlack,
+      surface: outerSpaceGrey,
+      onSurface: outerSpaceGrey,
+      onBackground: raisinBlack,
+      brightness: Brightness.dark);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-          colorScheme: ColorScheme(
-              primary: midnightGreenLight,
-              error: pink,
-              onError: pink,
-              secondary: midnightGreen,
-              onPrimary: midnightGreen,
-              onSecondary: midnightGreenLight,
-              background: raisinBlack,
-              surface: outerSpaceGrey,
-              onSurface: outerSpaceGrey,
-              onBackground: raisinBlack,
-              brightness: Brightness.dark),
+          scaffoldBackgroundColor: raisinBlack,
+          colorScheme: appColorScheme,
           textTheme: GoogleFonts.poppinsTextTheme(ThemeData.dark().textTheme)),
       title: 'SpendSense',
-      initialRoute: '/intro',
+      initialRoute: '/auth',
       routes: {
+        '/auth': (context) => AuthPage(),
         '/intro': (context) => const IntroPage1(),
         '/login': (context) => const LoginScreen(),
         '/register': (context) => const RegisterScreen(),
---
+++ lib/screens/Auth/auth.dart
@@ -0,0 +1,30 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutterati_codeshastra/screens/Home/home.dart';
+
+import 'package:flutterati_codeshastra/screens/Onboarding/intro_page_1.dart';
+
+class AuthPage extends StatefulWidget {
+  const AuthPage({super.key});
+
+  @override
+  State<AuthPage> createState() => _AuthPageState();
+}
+
+class _AuthPageState extends State<AuthPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return const Home();
+          } else {
+            return const IntroPage1();
+          }
+        },
+      ),
+    );
+  }
+}
